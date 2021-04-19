0be673edb52b454bc28bd512f037f271f9e9a3a4
vaibhavgupta-hike <vaibhavg@hike.in>
2021-04-19 19:46:35 +0530

    Working code for screenshot capture of 3d hikemoji on server side

---
+++ main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+const fs = require('fs');
+
+// app.disableHardwareAcceleration();
+
+var numTimesPaintCalled = 0;
+var startTime = new Date().getTime();
+
+app.on('ready', function() {
+
+    var win = new BrowserWindow({
+        width: 1280,
+        height:720,
+        webPreferences: { offscreen: true }
+    });
+    win.loadFile('webgl_loader_3dmoji.html');
+    win.webContents.on('paint', (event, dirty, image) => {
+        numTimesPaintCalled += 1;
+        var timeElapsed = new Date().getTime() - startTime;
+        console.log('Paint called', numTimesPaintCalled, 'times in ', timeElapsed, 'miliseconds.');
+
+        fname = 'ex' + String(numTimesPaintCalled) + '.png'
+        fs.writeFileSync(fname, image.toPNG())
+    });
+    win.webContents.setFrameRate(60);
+
+});
