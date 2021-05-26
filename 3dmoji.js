1e979d36c2567fd1bfb07bd42f210e1df969b8d0
Vaibhav Gupta <vaibhavg@hike.in>
2021-05-26 16:36:48 +0530

    Cleaned 3dmoji js

--- 3dmoji.js
+++ 3dmoji.js
@@ -1,210 +1,146 @@
-import * as THREE from './three.module.js';
-
-import { OrbitControls } from './OrbitControls.js';
-import { GLTFLoader } from './GLTFLoader.js';
+import * as THREE from './three.module.js'
+import { RGBELoader } from './RGBELoader.js'
 import { FBXLoader } from './FBXLoader.js'
-import { RGBELoader } from './RGBELoader.js';
-import { RoughnessMipmapper } from './RoughnessMipmapper.js';
-import { GUI } from './dat.gui.module.js';
+import { OrbitControls } from './OrbitControls.js'
 
 
-let camera, scene, renderer, hikemoji3d, mixer, container;
-const clock = new THREE.Clock();
 
-let numFrames = 0
 
-init();
-animate();
+const clock = new THREE.Clock()
+clock.start()
+const scene = new THREE.Scene()
 
-function initLights() {
-	const filler_left = new THREE.PointLight( 0xE1EFFF, 0.1, 13.65 );
-	filler_left.position.set(-0.3, -0.35, 1.04);
-	scene.add( filler_left );
+const container = document.createElement( 'div' );
+document.body.appendChild(container);
 
-	const filler_right = new THREE.PointLight( 0xE1EFFF, 0.1, 13.65 );
-	filler_right.position.set(0.78, -0.35, 1.04);
-	scene.add( filler_right );
+const filler_left = new THREE.PointLight( 0xE1EFFF, 0.1, 13.65 );
+filler_left.position.set(-0.3, -0.35, 1.04);
+scene.add( filler_left );
 
-	const filler_up = new THREE.PointLight( 0xE1EFFF, 0.1, 13.65 );
-	filler_up.position.set( 0.232, 0.3, 1.18);
-	scene.add( filler_up );
+const filler_right = new THREE.PointLight( 0xE1EFFF, 0.1, 13.65 );
+filler_right.position.set(0.78, -0.35, 1.04);
+scene.add( filler_right );
 
-	const key_center = new THREE.DirectionalLight( 0xffffff, 0.99 );
-	key_center.position.set(0.194, -0.72, 3.251);
-	key_center.rotation.set(-0.04, -180, 39.5);
-	scene.add( key_center );
-}
+const filler_up = new THREE.PointLight( 0xE1EFFF, 0.1, 13.65 );
+filler_up.position.set( 0.232, 0.3, 1.18);
+scene.add( filler_up );
 
-function initHikeMojiModel() {
+const key_center = new THREE.DirectionalLight( 0xffffff, 0.99 );
+key_center.position.set(0.194, -0.72, 3.251);
+key_center.rotation.set(-0.04, -180, 39.5);
+scene.add( key_center );
 
-	const roughnessMipmapper = new RoughnessMipmapper( renderer );
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+camera.position.set(0.1,  0.09999999999999995,1.8800000000000001)
+camera.rotation.set(-0.048039932847762506, 0.04798459447086554, 0.0023060623704482023)
+camera.fov = 47;
 
-	const loader = new FBXLoader().setPath('models/fbx/Female_Anim_Textures_4k/')
-	loader.load( 'Female_Anim_WithoutEmbed.fbx', function ( object ) {
-		hikemoji3d = object
-		hikemoji3d.position.set(0.14, -0.705, 0)
-		hikemoji3d.scale.set(0.0053, 0.0053, 0.0053)
 
-		console.log('fbx:', object)
-		// Turn the controllers off
-		hikemoji3d.children[0].children[0].visible = false
+function loadFile(path, loader) {
+	return new Promise((resolve, reject) => {
 
-		var female_lod = hikemoji3d.children[0].children[0].children[0]
-		console.log('female_lod:', female_lod)
-
-		var basebody_geo = female_lod.children[0].children[0]
-		console.log('basebody_geo:', basebody_geo)
-
-		const body_tex_loader = new THREE.TextureLoader();
-		body_tex_loader.load('')
-
-		mixer = new THREE.AnimationMixer( object );
-		const action = mixer.clipAction( object.animations[ 0 ] );
-		action.play();
-		object.traverse( function ( child ) {
-			if ( child.isMesh ) {
-				child.castShadow = true;
-				child.receiveShadow = true;
-			}
+		loader.load( path, function ( object ) {
+			resolve(object)
+		},
+		undefined,
+		(error) => {
+			reject(error)
 		} );
-		scene.add( object );
-		console.log('Hikemoji loaded in', clock.getElapsedTime(), 'seconds')
-	} );
 
-	// const loader = new GLTFLoader().setPath( 'models/gltf/MaleLODA_Rig_V02_Flip_out/' );
-	// loader.load( 'MaleLODA_Rig_V02_Flip.gltf', function ( gltf ) {
-
-	// 	hikemoji3d = gltf;
-	// 	hikemoji3d.scene.children[0].children[0].position.set(0.14, -0.705, 0);
-	// 	hikemoji3d.scene.children[0].children[0].scale.set(0.53, 0.53, 0.53);
-
-	// 	var moji_scene = gltf.scene;
-	// 	moji_scene.traverse((child) => {
-	// 	  if (! child.isMesh) return;
-	// 	  var prevMaterial = child.material;
-	// 	  child.material = new THREE.MeshPhongMaterial();
-	// 	  THREE.MeshStandardMaterial.prototype.copy.call( child.material, prevMaterial );
-	// 	  roughnessMipmapper.generateMipmaps( child.material );
-	// 	});
-	// 	// moji_scene.traverse(node => {
-	// 	// 	if(node.isMesh) {
-	// 	// 		console.log("traversing node ", node);
-	// 	// 		node.castShadow = false;
-	// 	// 		node.material.shininess=1000
-	// 	// 		node.material.metalness=1.5
-	// 	// 	}
-	// 	// });
-
-	// 	mixer = new THREE.AnimationMixer(moji_scene);
-
-	// 	var action = mixer.clipAction( gltf.animations[0] );
-	// 	action.setLoop(THREE.LoopOnce);
-	// 	action.clampWhenFinished = true;
-	// 	action.enable = true;
-	// 	action.play();
-
-	// 	console.log("gltf.animations: ", gltf.animations);
-
-	// 	scene.add( moji_scene );
-	// 	console.log('Hikemoji model loaded in', clock.getElapsedTime());
-	// 	render(0);
-	// 	console.log('Hikemoji model loaded in', clock.getElapsedTime());
-
-	// 	roughnessMipmapper.dispose();
-	// 	downloadGif()
-	// } );
+	})
 }
 
-function initRenderer() {
-	renderer = new THREE.WebGLRenderer( { canvas: canvas, alpha: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.toneMapping = THREE.ACESFilmicToneMapping;
-	renderer.toneMappingExposure = 1;
-	renderer.outputEncoding = THREE.sRGBEncoding;
-	container.appendChild( renderer.domElement );
+let hikemoji
+function loadHikemoji() {
+	return new Promise(async (resolve, reject) => {
+		const loader = new FBXLoader().setPath('models/fbx/Female_Anim_Textures_4k/')
+		hikemoji = await loadFile('Female_Anim_WithoutEmbed.fbx', loader)
+
+		hikemoji.position.set(0.14, -0.705, 0)
+		hikemoji.scale.set(0.0053, 0.0053, 0.0053)
+
+		console.log('hikemoji:', hikemoji)
+		// Turn the controllers off
+		hikemoji.children[0].children[0].visible = false
+
+		// Incomplete. But these objects need to be used to load textures manually
+		var female_lod = hikemoji.children[0].children[0].children[0]
+		var basebody_geo = female_lod.children[0].children[0]
+
+		const texLoader = new THREE.TextureLoader().setPath('textures/Female_2K_1K_0.5K/1K/')
+		const body_diffuse_tex = await loadFile('Female_1K_body_Diffuse.png', texLoader)
+		basebody_geo.map = body_diffuse_tex
+
+		const body_normal_tex = await loadFile('Female_1K_body_Normal.png', texLoader)
+		basebody_geo.normalMap = body_normal_tex
+
+
+		scene.add(hikemoji)
+		console.log('Hikemoji loaded')
+		resolve()
+	})
 }
 
-function initCamera() {
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-	camera.position.set( 0.1, 0.1, 1.88 );
-	camera.rotation.set( 0, -180, 0);
-	camera.fov = 47;
+await loadHikemoji()
+
+const duration = hikemoji.animations[0].duration
+const fps = 30
+const numFrames = Math.floor(duration * fps)
+
+const numWorkers = 4
+const framesPerWorker = Math.ceil(numFrames / numWorkers)
+
+const canvas = document.getElementById( 'canvas' );
+
+const renderer = new THREE.WebGLRenderer( { canvas: canvas, alpha: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding;
+container.appendChild(renderer.domElement);
+
+function screenshot(fname, renderer) {
+	return new Promise((resolve, reject) => {
+		console.log('camera position', camera.position)
+		console.log('camera rotation', camera.rotation)
+		renderer.render(scene, camera)
+		let base64String = renderer.domElement.toDataURL()
+		let base64Image = base64String.split(';base64,').pop();
+			fs.writeFile(fname, base64Image, {encoding: 'base64'}, function(err) {
+				console.log('File created');
+				resolve()
+		});
+	})
 }
 
-function initFloor() {
-	const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-	mesh.rotation.x = - Math.PI / 2;
-	scene.add( mesh );
+var rgbeLoader = new RGBELoader().setDataType(THREE.UnsignedByteType).setPath('textures/equirectangular/')
+const bgTexture = await loadFile('royal_esplanade_1k.hdr', rgbeLoader)
 
-	const grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
-	grid.material.opacity = 0.2;
-	grid.position.y = -0.705;
-	scene.add( grid );
-}
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+pmremGenerator.compileEquirectangularShader();
+const envMap = pmremGenerator.fromEquirectangular( bgTexture ).texture;
 
-function init() {
+scene.background = envMap;
+scene.environment = envMap;
 
-	container = document.createElement( 'div' );
-	document.body.appendChild( container );
+bgTexture.dispose();
+pmremGenerator.dispose();
+console.log('background texture loaded in time', clock.getElapsedTime())
 
-	scene = new THREE.Scene();
-	new RGBELoader()
-		.setDataType( THREE.UnsignedByteType )
-		.setPath( 'textures/equirectangular/' )
-		.load( 'royal_esplanade_1k.hdr', function ( texture ) {
+const controls = new OrbitControls( camera, renderer.domElement )
+controls.minDistance = 2
+controls.maxDistance = 10
+controls.target.set( 0, 0, -0.2)
+controls.update()
 
-			const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-
-			scene.background = envMap;
-			scene.environment = envMap;
-
-			texture.dispose();
-			pmremGenerator.dispose();
-	} );
-
-	const canvas = document.getElementById( 'canvas' );
-	initRenderer();
-	initCamera();
-	initLights();
-	initHikeMojiModel();
-	initFloor();
-
-	const pmremGenerator = new THREE.PMREMGenerator( renderer );
-	pmremGenerator.compileEquirectangularShader();
-
-	const controls = new OrbitControls( camera, renderer.domElement );
-	controls.minDistance = 2;
-	controls.maxDistance = 10;
-	controls.target.set( 0, 0, -0.2);
-	controls.update();
-
-	window.addEventListener( 'resize', onWindowResize );
-
-}
-
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-}
+const mixer = new THREE.AnimationMixer(hikemoji)
+const action = mixer.clipAction(hikemoji.animations[0])
+action.play()
 
 function animate() {
-	requestAnimationFrame(animate);
-
-	const delta = clock.getDelta();
-	render(delta);
-	if(mixer) mixer.update(delta);
+	requestAnimationFrame(animate)
+	mixer.update(clock.getDelta())
+	renderer.render(scene, camera)
 }
-
-function render(progress) {
-	// if(hikemoji3d) {
-	// 	hikemoji3d.scene.rotation.y += progress;
-	// }
-	renderer.render( scene, camera );
-	console.log('Time:', clock.getElapsedTime())
-
-	numFrames += 1
-	if(numFrames == 151) window.close()
-	// console.log("Ran in ", clock.getElapsedTime(), 'time')
-}
+requestAnimationFrame(animate)
